b4a7a8fb964e63a8664e3c3202e9bdaeb8e3710b
Nadirbiy1 <mr.ermatov04@gmail.com>
2025-03-13 21:05:34 +0600

    11.03 тапшырма

--- home_works.py
+++ home_works.py
@@ -1,34 +1,34 @@
-# # 1
-# res = [i for i in range(1, 21) if i % 2 == 0]
-# print(res)
+# 1
+res = [i for i in range(1, 21) if i % 2 == 0]
+print(res)
+
+# 2
+res = [i ** 2 for i in range(1, 11)]
+print(res)
+
+# 3
+n = [5, 12, 7, 18, 3, 10, 8]
+res = [i for i in n if i > 7]
+print(res)
+ # 3_2
 #
-# # 2
-# res = [i ** 2 for i in range(1, 11)]
-# print(res)
-#
-# # 3
-# n = [5, 12, 7, 18, 3, 10, 8]
-# res = [i for i in n if i > 7]
-# print(res)
-#  # 3_2
-# #
-# print([int(i)for i in input().split(",") if i > 7])
-# # 4
-# word = ["алма", "банан", "вишня"]
-# upper_word = [i.upper() for i in word]
-# print(upper_word)
-#
-# # 5
-# number = float(input("Санды киргизиңиз: "))
-# res = "оң" if number > 0 else "терс" if number < 0 else "нөл"
-# print(res)
-#
-# print(["on" if int(input()) > 0 else "Ters"])
-#
-# # 6
-# number = int(input("Санды киргизиңиз: "))
-# res = "жуп" if number % 2 == 0 else "так"
-# print(res)
+print([int(i)for i in input().split(",") if i > 7])
+# 4
+word = ["алма", "банан", "вишня"]
+upper_word = [i.upper() for i in word]
+print(upper_word)
+
+# 5
+number = float(input("Санды киргизиңиз: "))
+res = "оң" if number > 0 else "терс" if number < 0 else "нөл"
+print(res)
+
+print(["on" if int(input()) > 0 else "Ters"])
+
+# 6
+number = int(input("Санды киргизиңиз: "))
+res = "жуп" if number % 2 == 0 else "так"
+print(res)
 
 # 7
 numbers = [4, -1, 7, -3, 0, 9, -2]
