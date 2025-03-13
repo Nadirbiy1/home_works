4ccd324f9e655b076bb5b95567e5eca076867914
Nadirbiy1 <mr.ermatov04@gmail.com>
2025-03-13 19:48:11 +0600

    уй тапшырма 11.03

--- home_works.py
+++ home_works.py
@@ -1,32 +1,36 @@
-# 1
-res = [i for i in range(1, 21) if i % 2 == 0]
-print(res)
-
-# 2
-res = [i ** 2 for i in range(1, 11)]
-print(res)
-
-# 3
-n = [5, 12, 7, 18, 3, 10, 8]
-res = [i for i in n if i > 7]
-print(res)
-
-# 4
-word = ["алма", "банан", "чие"]
-upper_word = [i.upper() for i in word]
-print(upper_word)
-
-# 5
-number = float(input("Санды киргизиңиз: "))
-res = "оң" if number > 0 else "терс" if number < 0 else "нөл"
-print(res)
-
-# 6
-number = int(input("Санды киргизиңиз: "))
-res = "жуп" if number % 2 == 0 else "так"
-print(res)
+# # 1
+# res = [i for i in range(1, 21) if i % 2 == 0]
+# print(res)
+#
+# # 2
+# res = [i ** 2 for i in range(1, 11)]
+# print(res)
+#
+# # 3
+# n = [5, 12, 7, 18, 3, 10, 8]
+# res = [i for i in n if i > 7]
+# print(res)
+#  # 3_2
+# #
+# print([int(i)for i in input().split(",") if i > 7])
+# # 4
+# word = ["алма", "банан", "вишня"]
+# upper_word = [i.upper() for i in word]
+# print(upper_word)
+#
+# # 5
+# number = float(input("Санды киргизиңиз: "))
+# res = "оң" if number > 0 else "терс" if number < 0 else "нөл"
+# print(res)
+#
+# print(["on" if int(input()) > 0 else "Ters"])
+#
+# # 6
+# number = int(input("Санды киргизиңиз: "))
+# res = "жуп" if number % 2 == 0 else "так"
+# print(res)
 
 # 7
 numbers = [4, -1, 7, -3, 0, 9, -2]
-updated_numbers = [0 if i < 0 else i for i in numbers]
-print(updated_numbers)
+numbers = [0 if i < 0 else i for i in numbers]
+print(numbers)
