21236c2766e5e6547c9bed942a2642d141d1fc04
Nadirbiy1 <mr.ermatov04@gmail.com>
2025-03-13 20:09:21 +0600

    Set тиби жонундо тушунук

---
+++ home_works.py
@@ -0,0 +1,22 @@
+# set - множества
+nums = {1, 1, 1, 2, 2, 3, 3, 3}
+names = {
+    "Arsen",
+    "Grad",
+    "Kanat",
+    "Shergazy",
+    "Ernis",
+    "Aida",
+    "Aiana"
+}
+name = input("Enter your name: ")
+
+print(names)
+names.add(name)
+print(names)
+
+
+
+
+
+
